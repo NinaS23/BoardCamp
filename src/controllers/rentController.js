548dca6cddaffb32c7931456568ee5ea33cc7ac8
MariaDuda <mariabevenuto062@gmail.com>
2022-07-22 11:35:00 -0300

    fix getRentals

--- src/controllers/rentController.js
+++ src/controllers/rentController.js
@@ -4,20 +4,41 @@ import connection from "../database.js";
 export async function getRents(req, res) {
     const { customerId, gameId } = req.query
     try {
-        if (!customerId && !gameId) {
-            const rentals = await connection.query('SELECT * FROM rentals;');
-            return res.status(200).send(rentals.rows);
-        } 
-        if(gameId) { 
-            const gameId = await connection.query(`
-            SELECT * FROM rentals WHERE gameId LIKE '${gameId}%';`);
-            return res.status(200).send(gameId.rows);
+        const { rows: rentals } = await connection.query(`
+     SELECT rentals.*,
+        customers.id AS "customerId",
+        customers.name AS "customerName",
+        games.id AS "gameId",
+        games.name AS "gameName", 
+        categories.id AS "categoryId",
+        categories.name AS "categoryName"
+     FROM rentals
+        JOIN customers ON rentals."customerId" = customers.id
+        JOIN games ON rentals."gameId" = games.id
+        JOIN categories ON categories.id = games."categoryId"
+        ${customerId ? `WHERE customers.id = ${parseInt(customerId)}` : ""}
+        ${gameId ? `WHERE games.id = ${parseInt(gameId)}` : ""}
+        `)
+        
+        const listRentals = [] 
+
+        for(let rent of rentals){//variável of interável
+            rent = {
+                ...rent,
+                customer: {
+                    id: rent.customerId,
+                    name: rent.customerName
+                },
+                game: {
+                    id: rent.gameId,
+                    name: rent.gameName,
+                    categoryId: rent.categoryId,
+                    categoryName: rent.categoryName
+                }
+            }
+            listRentals.push(rent)
         }
-        if(customerId) {
-            const customerId = await connection.query(`
-        SELECT * FROM rentals WHERE customerId LIKE '${customerId}%';`);
-            return res.status(200).send(customerId.rows);
-        } 
+        res.status(200).send(listRentals);
     } catch (e) {
         console.log(e)
         res.sendStatus(500);
